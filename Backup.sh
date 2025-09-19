e19c36518147ff8829b85671888416c9aa5a02fe
Harshal Mali <139382537+harshalmali042@users.noreply.github.com>
2025-09-19 10:04:45 +0530

    Update Backup.sh

--- Backup.sh
+++ Backup.sh
@@ -7,14 +7,14 @@ BACKUP_DIR="/backups/data"
 LOG_FILE="/var/log/backup.log"
 DATE=$(date +%F_%T)
 
-echo "===== Backup started at $DATE =====" | tee -a $LOG_FILE
+echo " Backup started at $DATE " | tee -a "$LOG_FILE"
 
-rsync -avz --delete $SOURCE_DIR ${USER}@${HOST_IP}:${BACKUP_DIR} >> $LOG_FILE 2>&1
+rsync -avz --delete "$SOURCE_DIR" "${USER}@${HOST_IP}:${BACKUP_DIR}" >> "$LOG_FILE" 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "$(date): Backup Successful" | tee -a $LOG_FILE
+    echo "$(date): Backup Successful" | tee -a "$LOG_FILE"
 else
-    echo "$(date): Backup Failed!" | tee -a $LOG_FILE
+    echo "$(date): Backup Failed!" | tee -a "$LOG_FILE"
 fi
 
-echo " Backup completed at $(date +%F_%T) " | tee -a $LOG_FILE
+echo " Backup completed at $(date +%F_%T) " | tee -a "$LOG_FILE"
