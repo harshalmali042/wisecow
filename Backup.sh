e23b7f3e610864eb8c3917b45fe123011302c206
Harshal Mali <139382537+harshalmali042@users.noreply.github.com>
2025-09-19 10:00:46 +0530

    Create Backup.sh

---
+++ Backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR="/home/Ubuntu/data"
+USER="Ubuntu"
+HOST_IP="192.168.1.100"
+BACKUP_DIR="/backups/data"
+LOG_FILE="/var/log/backup.log"
+DATE=$(date +%F_%T)
+
+echo "===== Backup started at $DATE =====" | tee -a $LOG_FILE
+
+rsync -avz --delete $SOURCE_DIR ${USER}@${HOST_IP}:${BACKUP_DIR} >> $LOG_FILE 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "$(date): Backup Successful" | tee -a $LOG_FILE
+else
+    echo "$(date): Backup Failed!" | tee -a $LOG_FILE
+fi
+
+echo " Backup completed at $(date +%F_%T) " | tee -a $LOG_FILE
